996e956de7b0ed709d2914dd198785b1deb73d00
Chris <christophercrowd43@gmail.com>
2021-03-18 15:45:44 -0400

    feat: Added Routing, Auth and not-found view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NotFound from "./views/404";
+import Assets from "./views/Assets";
+import Auth from "./views/Auth";
+import Budget from "./views/Budget";
+import Home from "./views/Home";
+import Liability from "./views/Liability";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Auth} />
+        <Route path="/home" component={Home} />
+        <Route path="/budget" component={Budget} />
+        <Route path="/assets" component={Assets} />
+        <Route path="/liabilties" component={Liability} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
