521ed3beaef0a5440cafb2843ab5bc02b2c6890b
Chris <christophercrowd43@gmail.com>
2021-03-20 18:02:57 -0400

    feat: Added Layout, for app

--- src/App.js
+++ src/App.js
@@ -9,14 +9,19 @@ import Liability from "./views/Liability";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Auth} />
-        <Route path="/home" component={Home} />
-        <Route path="/budget" component={Budget} />
-        <Route path="/assets" component={Assets} />
-        <Route path="/liabilties" component={Liability} />
-        <Route path="*" component={NotFound} />
-      </Switch>
+      <div className="app">
+        <div className="side-nav">Navbar</div>
+        <div className="main">
+          <Switch>
+            <Route path="/" exact component={Auth} />
+            <Route path="/home" component={Home} />
+            <Route path="/budget" component={Budget} />
+            <Route path="/assets" component={Assets} />
+            <Route path="/liabilties" component={Liability} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </div>
+      </div>
     </Router>
   );
 }
